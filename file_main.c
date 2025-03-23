d3cc6b9c5ea62802271927184295ba8d31ab9f5f
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2025-03-23 00:45:03 -0400

    Adding Write, Append, and Reading Functionality. Made a note on adding a new line when appending and attempting to use a while loop to read the content of the file into a string of characters.

--- file_main.c
+++ file_main.c
@@ -6,8 +6,27 @@ int main()
 
     printf("Hello and welcome to the File Handling Program for C");
 
-    filePTR = fopen(".\\Saved_Files\\test.txt", "w");
+    //filePTR = fopen(".\\Saved_Files\\test.txt", "w");
 
+    // filePTR = fopen(".\\Saved_Files\\test.txt", "a");
+
+    // Write Text to the File
+    // fprintf(filePTR, "First time writing to text");
+    // fprintf(filePTR, "First time appending to text");
+
+    // Make sure to add \n when adding text or it will print on the same line
+    // fprintf(filePTR, "\nAdding a new line");
+
+    filePTR = fopen(".\\Saved_Files\\test.txt", "r");
+
+    // 500 Characters for each file max
+    char fileString[500];
+
+    // Use fgets function to get the job done
+    while (fgets(fileString, 500, filePTR))
+    {
+        printf("%s", fileString);
+    }
     fclose(filePTR);
 
     return 0;
